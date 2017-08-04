d26f7a41f3c89c9f1408afa632bdbc16eb8503a1
diegodifreitas <diegodifreitas@gmail.com>
2017-08-04 15:23:45 -0300

    Alter style-guide

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-const msg = 'algoritmo para contagem do numero de caracter em uma msg'
+const msg = 'algoritmo para contagem do numero de caracter em uma msg';
 const arrayMsg = msg.split('').filter(e => e !== ' ')
 
 const resultMsg = arrayMsg.map((symbol) => {
   let number = 0
-  arrayMsg.forEach((s) => { (s === symbol) && (number += 1) })
+  arrayMsg.forEach((s) => { if (s === symbol) (number += 1) })
   return {
     symbol,
     qtd: number,
@@ -14,9 +14,10 @@ const resulOrder = resultMsg.filter(x => x.qtd > 0).sort((a, b) => b.qtd - a.qtd
 
 const uniq = (a, param) => (
   a.filter((item, pos, array) => (
-    array.map(mapItem => mapItem[param]).indexOf(item[param]) === pos),
-  )
+    array.map(mapItem => (mapItem[param]).indexOf(item[param]) === pos)
+  ))
 )
 
 const result = uniq(resulOrder, 'symbol')
+
 console.log(result)
