7807ae5b337d09496f7da5921ab23affe2c916f4
diegodifreitas <diegodifreitas@gmail.com>
2017-08-04 18:01:53 -0300

    Alter main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 const msg = 'algoritmo para contagem do numero de caracter em uma msg';
-const arrayMsg = msg.split('').filter(e => e !== ' ')
+const arrayMsg = [...msg].filter(e => e !== ' ')
 
 const resultMsg = arrayMsg.map((symbol) => {
   let number = 0
-  arrayMsg.forEach((s) => { if (s === symbol) (number += 1) })
+  arrayMsg.forEach((s) => { if (s.includes(symbol)) (number += 1) })
   return {
     symbol,
     qtd: number,
   }
 })
 
-const resulOrder = resultMsg.filter(x => x.qtd > 0).sort((a, b) => b.qtd - a.qtd)
+const resulOrder = resultMsg.sort((a, b) => b.qtd - a.qtd)
 
-const uniq = (a, param) => (
+const uniq = (a = [], param) => (
   a.filter((item, pos, array) => (
-    array.map(mapItem => (mapItem[param]).indexOf(item[param]) === pos)
-  ))
+    array.map(mapItem => mapItem[param]).indexOf(item[param]) === pos),
+  )
 )
 
 const result = uniq(resulOrder, 'symbol')
