1f114bfb4c7105dd0b40f9d2b89cf249870bb9d7
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-07-25 18:02:17 -0300

    Added sass-loader in webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,13 +8,20 @@ module.exports = {
     filename: "principal.js",
     path: __dirname + "/public"
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css"
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s?[ac]ss$/, //css - sass
         use: [
-          "style-loader", //Adiciona CSS a DOM injetando a tag <style>
-          "css-loader" // interpreta @import, url()...
+          MiniCssExtractPlugin.loader,
+          //"style-loader", //Adiciona CSS a DOM injetando a tag <style>
+          "css-loader", // interpreta @import, url()...
+          "sass-loader"
         ]
       }
     ]
