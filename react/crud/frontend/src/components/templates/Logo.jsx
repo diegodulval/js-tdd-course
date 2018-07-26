20e295648c711ddc4c7fb33635bdd3d1f5af0a2d
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-07-26 17:43:01 -0300

    Created routes and method for save user

--- react/crud/frontend/src/components/templates/Logo.jsx
+++ react/crud/frontend/src/components/templates/Logo.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Logo.css";
 
 import logo from "../../assets/imgs/logo.png";
 
 export default props => (
   <aside className="logo">
-    <a href="/" className="logo">
+    <Link to="/" className="logo">
       <img src={logo} alt="Logo" />
-    </a>
+    </Link>
   </aside>
 );
--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 
+import Routes from "./Routes";
 import Nav from "../components/templates/Nav";
 import Footer from "../components/templates/Footer";
 import Logo from "../components/templates/Logo";
-import Home from "../components/home/Home";
 
 export default props => (
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Home />
-    <Footer />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </BrowserRouter>
 );
